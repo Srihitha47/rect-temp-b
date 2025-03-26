069860cd858094062c834127eade4a3e3d6d6758
Srihitha47 <srihitha0475@gmail.com>
2025-03-26 15:18:45 +0530

    commit

--- src/components/App13.jsx
+++ src/components/App13.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
-
+import React from "react";
+import { useRef, useState } from "react";
 export default function App13() {
+  const msgRef = useRef();
+  const [color, setColor] = useState();
+  const handleSubmit = () => {
+    msgRef.current.style.color = color
+  };
   return (
-    <div>App13</div>
-  )
+    <div>
+      <h3>This is App13</h3>
+      <p>
+        <input type="text" onChange={(e)=>setColor(e.target.value)}></input>
+        <button onClick={handleSubmit}>Submit</button>
+      </p>
+      <p>
+        <h4 ref={msgRef}>Hello World</h4>
+      </p>
+    </div>
+  );
 }
