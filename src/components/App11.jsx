d4de76f3208906e37d95392ea001a83df079acee
Srihitha47 <srihitha0475@gmail.com>
2025-03-26 11:07:49 +0530

    commit

--- src/components/App11.jsx
+++ src/components/App11.jsx
@@ -1,9 +1,19 @@
 import React from "react";
-import { useState } from "react";
+import { useState , useEffect} from "react";
+import "./App11.css"
 export default function App11() {
   const [count, setCount] = useState(0);
     const [count1, setCount1] = useState(0);
-    const [message, setMessage] = useState("");
+    const [message, setMessage] = useState("Let's Begin The Game");
+
+    // useEffect(()=>{
+    //     if count>0 && setMessage("well Done!");
+    // },[count]);
+
+    // useEffect(()=>{
+        // count1 1> 0 && setMessage("Better Luck Nxt Time");
+    // },[count1]);
+
     const increment = () => {
         setCount(count+1)
         setMessage("Well done!")
@@ -11,19 +21,27 @@ export default function App11() {
         
       };
       const decrement = () => {
-        setCount1(count1+1)
+        count1 < 10 && setCount1(count1+1)
         setMessage("Better luck Next Time!!")
        
       };
       return (
-        <div>
+        <div className="App-App11-Container">
+            <h3>This is App11</h3>
+            <div className="App-App11-Row">
+            <div className="App-App11-Box">
           <button onClick={decrement}>Wicket</button>
-          {count1}
-    
+          <p>{count1}</p>
+          </div>
+          <div className="App-App11-Box">
           <button onClick={increment}>run</button>
-          {count}
-
-          <p>{message}</p>
+          <p>{count}</p>
+          </div>
+          </div>
+          <hr></hr>
+          <div className="App-App11-Row">
+          <p><center>{message}</center></p>
+          </div>
         </div>
       );
     }
