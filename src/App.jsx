528002f39e00342d4938bf6291e867ca1a13715e
Srihitha47 <srihitha0475@gmail.com>
2025-04-04 14:58:19 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ import App19 from "./components/App19";
 import App20 from "./components/App20";
 import App21 from "./components/App21";
 import App22 from "./components/App22";
+import App23 from "./components/App23";
 function App() {
   return (
     <BrowserRouter>
@@ -48,6 +49,7 @@ function App() {
           <Link to="app20" className="box">App20</Link>
           <Link to="app21" className="box">App21</Link>
           <Link to="app22" className="box">App22</Link>
+          <Link to="app23" className="box">App23</Link>
         </div>
         <hr></hr>
         <div>
@@ -75,6 +77,7 @@ function App() {
             <Route path="app20" element={<App20 />} />
             <Route path="app21" element={<App21 />} />
             <Route path="app22" element={<App22 />} />
+            <Route path="app23" element={<App23 />} />
           </Routes>
         </div>
       </div>
