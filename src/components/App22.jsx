5868f2b20da1ba572f97d6ad9a8d8c4980afb2a9
Srihitha47 <srihitha0475@gmail.com>
2025-04-04 14:26:29 +0530

    commit

--- src/components/App22.jsx
+++ src/components/App22.jsx
@@ -1,25 +1,30 @@
-import React from 'react'
-import { useEffect,useState } from 'react';
+import React from "react";
+import { useEffect, useState } from "react";
+export default function App21() {
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState([]);
+  const url = "https://jsonplaceholder.typicode.com/users";
+  useEffect(() => {
+    console.log("Component Mounted");
+    fetch(url).then((res) => res.json().then((data) => setData(data)));
+    return () => {
+      console.log("Component Unmounted");
+    };
+  }, []);
 
-export default function App22() {
-    const[count,setCount]=useState(0);
-    useEffect(()=>{
-        console.log("Component Mounted");
-        return()=>{
-            console.log("Component Unmounted");
-        };
-
-    },[]);
-    const handleClick =()=>{
-        setCount(count+1)
-        console.log("component upadated or reloaded")
-
-    }
+  const handleClick = () => {
+    setCount(count + 1);
+    console.log("Component Updated / Reloaded");
+  };
   return (
     <div>
-        <h3>App22</h3>
-        <button onClick={handleClick}>Click</button>
-        {count}
-        </div>
+      <h3>This is App21</h3>
+      <button onClick={handleClick}>Click</button>
+      <p>{count}</p>
+      <hr></hr>
+      {data.map((value,index)=>(
+        <div key={index}>{value.name}-{value.email}</div>
+      ))}
+    </div>
   );
 }
