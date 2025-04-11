9c57d3f8f7022a70a4a946544ccc3657a78cf093
Srihitha47 <srihitha0475@gmail.com>
2025-04-11 10:06:26 +0530

    commit

--- src/components/App13.jsx
+++ src/components/App13.jsx
@@ -9,13 +9,14 @@ export default function App13() {
   return (
     <div>
       <h3>This is App13</h3>
+      <p><b><i>useRef hook</i></b>: <b>To change the color of a "Hello World" message dynamically,</b>
+      <br></br> this component uses `useState` to store the color input from a text field <br></br>and `useRef` to directly reference the h4 element displaying the message. 
+      <br></br>The `e.target.value` retrieves the current typed input. When you type a color into the input box and click the Submit button, the `handleSubmit` function applies that color to the text using inline styles via the ref.</p>
       <p>
-        <input type="text" onChange={(e)=>setColor(e.target.value)}></input>
+        <input type="text" onChange={(e) => setColor(e.target.value)}></input>
         <button onClick={handleSubmit}>Submit</button>
       </p>
-      <p>
         <h4 ref={msgRef}>Hello World</h4>
-      </p>
     </div>
   );
 }
--- src/components/App23.jsx
+++ src/components/App23.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 export default function App23() {
+    const NAME=import.meta.env.REACT_APP_NAME
   return (
-    <div>App23</div>
+    <div>
+        <h3>App23</h3>
+        {NAME}
+        </div>
   )
 }
